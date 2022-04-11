93efdd4ae7c4ebab5753d21b51f5c908a16344da
Eldrick-beaumont <eldrick.beaumont@mail.mcgill.ca>
2022-04-11 10:13:06 -0700

    RateReviewTAbyCourse update

--- matter/rateTA.php
+++ matter/rateTA.php
@@ -1,45 +1,120 @@
 <?php
 /*Variables used to rate a TA*/
-$course = '';
-$term = '';
-$TA = '';
-$rating = '';
-$comments = '';
+$studentid = "270856020";
+display("RateReviewaTAbyCourseTOP.txt");
 
-/*If the submit button is clicked*/
-if(isset($_POST["submit"])){
-  /*Store the input into the variables*/
-  $course = $_POST["course"];
-  $term = $_POST["term"];
-  $TA = $_POST["TA"];
-  $phone = $_POST["rating"];
-  $comments = $_POST["comments"];
-  /*Open the CSV file*/
-  $file_open = fopen("rateTA.csv", "a");
-  $no_rows = count(file("rateTA.csv"));
-  
-  if($no_rows > 1)
-  {
-   $no_rows = ($no_rows - 1) + 1;
-  }
-  /*Append the new TA rate*/
-    fwrite($file_open, $course);
-    fwrite($file_open, ", ");
-    fwrite($file_open, $term);
-    fwrite($file_open, ", ");
-    fwrite($file_open, $TA);
-    fwrite($file_open, ", ");
-    fwrite($file_open, $rating);
-    fwrite($file_open, ", ");
-    fwrite($file_open, $comments);
-    fwrite($file_open, "\r\n");
-  /*Reset the variables*/
-    $course = '';
-    $term = '';
-    $TA = '';
-    $rating = '';
-    $comments = '';
-  /*Close the file*/
-  fclose($file_open);
+if (sizeof($_GET)==0) {
+	echo "<form method=\"post\" action=\"rateTA.php?Page=getTA&Student=".$studentid."\">";
+
+	echo "<h2>Course</h2>";
+	echo "For Which Course do you want to rate a TA?<br>";
+	echo "<select name=\"course\">";
+    	echo "<option value=\"----------------------------------------------------------\" >----------------------------------------------------------</option>";
+    	
+	$courseArray = getCoursesForStudent($studentid);
+	foreach($courseArray as $row){
+		echo "<option value=\"" . $row['courseid'] . "\" >".$row['term_year']."-".$row['course_num']."-".$row['course_name']."</option>";
+	}
+
+    	echo "</select><br><br>";
+
 }
+else if ($_GET["Page"]=="getTA") {
+	$courseid = $_POST['course'];
+
+	echo "<form method=\"post\" action=\"rateTA.php?Page=rateTA&Student=".$studentid."&Course=".$courseid."\">";
+	echo"<h2>TA</h2>";
+	echo "Choose the TA you are rating?<br>";
+	echo "<select name=\"TA\">";
+	echo "<option value=\"----------------------------------------------------------\" >----------------------------------------------------------</option>";
+	
+	$TAarray =  getTAforCourse($courseid);
+	foreach($TAarray as $row){
+		echo "<option value=\"" . $row['taid'] . "\" >"."TA-".$row['taid']." ".$row['firstname']." ".$row[2]."</option>";
+	}
+
+	
+	echo "</select><br><br>";
+
+}
+
+else if ($_GET["Page"]=="rateTA") {
+	$taid = $_POST['TA'];
+	echo "<form method=\"post\" action=\"rateTA.php?Page=reviewSubmit&Student=".$studentid."&Course=".$_GET["Course"]."&TA=".$taid."\">";
+
+	display("RateReviewaTA.txt");
+}
+else if ($_GET["Page"]=="reviewSubmit") {
+	$courseid = $_GET['Course'];
+	$TAid = $_GET['TA'];
+	$rating = $_POST['rating'];
+	$review =$_POST['comment']; 
+	if(addTAreview($courseid,$TAid,$rating,$review)){
+		echo "SUBMITTED";
+	}
+	else{
+		echo "FAILLED";
+	}
+}
+
+if($_GET["Page"]!="reviewSubmit"){
+	display("RateReviewaTAbyCoursebottom.txt");
+}
+display("footer.txt");
+
+function display($path) {
+  $file = fopen($path,"r");
+  while(!feof($file)) {
+    $line = fgets($file);
+    echo $line;
+  }
+  fclose($file);
+}
+
+//Used to save a new TA review in the database
+//returns true if the review was added
+function addTAreview($courseid, $TAid, $rating, $review){
+	$pdo = new PDO("sqlite:" . "../DB/Main.db");
+	$maxreviewid = $pdo->query("SELECT MAX(reviewid) FROM TAreview");
+	$newreviewid = $maxreviewid->fetchColumn() + 1;
+
+	$query = $pdo->prepare("INSERT INTO TAreview (reviewid, taid, courseid, rating, review) VALUES (?,?,?,?,?)");
+	$err1 = $query->execute(array($newreviewid, $TAid, $courseid, $rating, $review));
+	
+	$pdo = null;
+	return ($err1 == 1);
+
+}
+
+//returns an array of rows (access each rows like this: foreach($arrray as $row){})
+//each row is a course taken by that student
+//each row is an array of this form $row = ['courseid', 'term_year', 'course_num', 'course_name']
+function getCoursesForStudent($studentid){
+
+	$pdo = new PDO("sqlite:" . "../DB/Main.db");
+
+	$query = $pdo->prepare("SELECT c.courseid, c.term_year, c.course_num, c.course_name 
+		FROM TakingCourse tc, Course c 
+		WHERE tc.studentid == ? and tc.courseid == c.courseid;");
+	
+	$query->execute(array($studentid));
+	
+	return $query->fetchAll();
+}
+
+
+//returns an array of rows (access each rows like this: foreach($arrray as $row){})
+//each row is a TA associated with that course
+//each row is an array of this form $row = ['taid', 'firstname', 'lastname']
+function getTAforCourse($courseid){
+	$pdo = new PDO("sqlite:" . "../DB/Main.db");
+
+	$query = $pdo->prepare("SELECT ta.taid, ta.firstname, ta.lastname 
+		FROM TA ta, AssistingCourse ac
+		WHERE ta.taid == ac.taid and ac.courseid == ?");
+	$query->execute(array($courseid));
+
+	return $query->fetchAll();
+}
+
 ?>
