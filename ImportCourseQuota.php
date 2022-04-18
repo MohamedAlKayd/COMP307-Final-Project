262465f69a40200f52fee8ffba156a4928d9597b
Eldrick BEAUMONT <ebeaum1@teaching.CS.McGill.CA>
2022-04-17 22:33:44 -0400

    Import for Administration

---
+++ ImportCourseQuota.php
@@ -0,0 +1,43 @@
+<?php
+    echo "QUOta";
+    $content = $_POST['con'];
+
+    $ar = explode("\n",$content); 
+
+    foreach($ar as $line){
+        $ar2 = explode(",",$line);
+        $term_year = $ar2[0];
+        $course_num = $ar2[1];
+        $course_type = $ar2[2];
+        $course_name = $ar2[3];
+        $instructor = $ar2[4];
+        $course_enrollment_num = $ar2[5];
+        $TA_quota = $ar2[6];
+    
+        addToCourseQuotaInfo($term_year,$course_num,$course_type,$course_name,$instructor,$course_enrollment_num,$TA_quota);
+    }
+
+    echo "END";
+
+    function addToCourseQuotaInfo($term_year,$course_num,$course_type,$course_name,$instructor_name,$course_enrollment_num,$TA_quota){
+        $pdo = new PDO("sqlite:" . "DB/Main.db");
+        $courseid = addThisCourse($term_year, $course_num, $course_name, $instructor_name);
+    
+        $query = $pdo->prepare("INSERT INTO CourseQuotaInfo (courseid,term_year,course_num,course_type,course_name,instructor_name,course_enrollment_num,TA_quota) VALUES (?,?,?,?,?,?,?,?)");
+        $err1 = $query->execute(array($courseid,$term_year,$course_num,$course_type,$course_name,$instructor_name,$course_enrollment_num,$TA_quota));
+    
+        return $err1 == 1;
+    }
+
+    function addThisCourse($term_year, $course_num, $course_name, $instructor_assigned_name){
+        $pdo = new PDO("sqlite:" . "DB/Main.db");
+        $maxCourseid = $pdo->query("SELECT MAX(courseid) FROM Course");
+        $newCourseid = $maxCourseid->fetchColumn() + 1;
+    
+        $coursequery = $pdo->prepare("INSERT INTO Course (courseid, term_year, course_num, course_name, instructor_assigned_name) VALUES (?,?,?,?,?)");
+        $err1 = $coursequery->execute(array($newCourseid, $term_year, $course_num, $course_name, $instructor_assigned_name));
+    
+        return $newCourseid;
+    }
+   
+?>
---
+++ ImportTACohort.php
@@ -0,0 +1,66 @@
+<?php
+    echo "CoHORT";
+    $content = $_POST['con'];
+
+    $ar = explode("\n",$content); 
+
+    foreach($ar as $line){
+        $ar2 = explode(",",$line);
+        $TA_name = $ar2[0];
+        $student_ID = $ar2[1];
+        $legal_name = $ar2[2];
+        $email = $ar2[3];
+        $grad_ugrad = $ar2[4];
+        $supervisor_name = $ar2[5];
+        $priority = $ar2[6];
+        $hours = $ar2[7];
+        $date_applied = $ar2[8];
+        $location = $ar2[9];
+        $phone = $ar2[10];
+        $degree = $ar2[11];
+        $courses_applied_for = $ar2[12];
+        $open_to_other_courses = $ar2[13];
+        $notes = $ar2[14];
+        addToTACohortInfo($TA_name,$student_ID,$legal_name,$email,$grad_ugrad,$supervisor_name,$priority,$hours,$date_applied,$location,$phone,$degree,$courses_applied_for,$open_to_other_courses,$notes);
+    }
+
+    echo "END";
+
+    function addToTACohortInfo($TA_name,$student_ID,$legal_name,$email,$grad_ugrad,$supervisor_name,$priority,$hours,$date_applied,$location,$phone,$degree,$courses_applied_for,$open_to_other_courses,$notes){
+        $namearray = explode(" ",$TA_name);
+        $firstname = $namearray[0];
+        $lastname = $namearray[1];
+        $username = $email;
+        $taid= addUserTA($firstname, $lastname, $username, $email);
+        
+        $pdo = new PDO("sqlite:" . "DB/Main.db");
+    
+        $query = $pdo->prepare("INSERT INTO TACohortInfo (taid,TA_name,student_ID,legal_name,email,grad_ugrad,supervisor_name,priority,hours,date_applied,location,phone,degree, courses_applied_for, open_to_other_courses, notes) 
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        $err1 = $query->execute(array($taid,$TA_name,$student_ID,$legal_name,$email,$grad_ugrad,$supervisor_name,$priority,$hours,$date_applied,$location,$phone,$degree,$courses_applied_for,$open_to_other_courses,$notes));
+    
+        return $err1 == 1;
+    }
+    
+    function addUserTA($firstname, $lastname, $username, $email){
+            
+        $pdo = new PDO("sqlite:" . "DB/Main.db");
+        $maxuserid = $pdo->query("SELECT MAX(userid) FROM User");
+        $newuserid = $maxuserid->fetchColumn() + 1;
+    
+        $maxUid = $pdo->query("SELECT MAX(taid) FROM TA");
+        $newUid = $maxUid->fetchColumn() + 1;
+        $query = $pdo->prepare("INSERT INTO TA (taid, userid, firstname, lastName, email) VALUES (?,?,?,?,?)");
+    
+        $userquery = $pdo->prepare("INSERT INTO User (userid, username) VALUES (?,?)");
+        $err1 = $userquery->execute(array($newuserid, $username));
+    
+        
+        $err2 = $query->execute(array($newUid, $newuserid, $firstname, $lastname, $email));
+        
+        $pdo = null;
+        return $newUid;
+    
+    }
+   
+?>
